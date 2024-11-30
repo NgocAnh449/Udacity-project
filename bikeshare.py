b851cc2661f866959bbe423599b3b83efcb56554
NgocAnh449 <anhntn.hust@gmail.com>
2024-11-30 23:03:56 +0700

    Restore deleted lines

--- bikeshare.py
+++ bikeshare.py
@@ -185,7 +185,15 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df, city)
+# Ask if the user wants to see raw data
+        see_data = input("\nWould you like to see some raw data? (yes/no): ").lower()
+        if see_data == 'yes':
+            display_data(df)
 
+        # Ask if the user wants to restart the analysis
+        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        if restart.lower() != 'yes':
+            break
         
 if __name__ == "__main__":
     main()
